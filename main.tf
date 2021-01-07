ab41c85206f687b120617b6045138be74e77f4db
bassbone <bassbone0o_o0@yahoo.co.jp>
2021-01-07 15:29:15 +0000

    variable

--- main.tf
+++ main.tf
@@ -1,15 +1,43 @@
 provider "google" {}
 
+variable "name" {
+  default = "instance-gpu"
+}
+
+variable "cpu" {
+  default = "2"
+}
+
+variable "memory" {
+  default = "8192"
+}
+
+variable "zone" {
+  default = "us-central1-b"
+}
+
+variable "disk_size" {
+  default = 50
+}
+
+variable "disk_image" {
+  default = "deeplearning-platform-release/pytorch-latest-gpu"
+}
+
+variable "preemptible" {
+  default = false
+}
+
 resource "google_compute_instance" "default" {
-  name         = "instance-gpu"
-  machine_type = "custom-2-8192"
-  zone         = "us-central1-b"
+  name         = var.name
+  machine_type = "custom-${var.cpu}-${var.memory}"
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-      size = 50
+      size = var.disk_size
       type = "pd-ssd"
-      image = "deeplearning-platform-release/pytorch-latest-gpu"
+      image = var.disk_image
     }
   }
 
@@ -23,7 +51,7 @@ resource "google_compute_instance" "default" {
   }
 
   scheduling {
-    preemptible = false
+    preemptible = var.preemptible
     automatic_restart = false
     on_host_maintenance = "TERMINATE"
   }
